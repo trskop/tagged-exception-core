388c91b1182fc9120e97f60c85bb136794c347ca
Peter Trško <peter.trsko@gmail.com>
2013-07-23 22:20:57 +0200

    [core] Introducing new specific unsafe lift operations
    liftBindLike
        :: (m a -> (b -> m c) -> m d)
        -> Throws e m a
        -> (b -> Throws e m c)
        -> Throws e m d
    
    liftFlipBindLike
        :: ((a -> m b) -> m c -> m d)
        -> (a -> Throws e m b) -> Throws e m c -> Throws e m d
    
    liftKleisliLike
        :: ((a -> m a') -> (b -> m b') -> c -> m c')
        -> (a -> Throws e m a')
        -> (b -> Throws e m b')
        -> c -> Throws e m c'

--- core/src/Control/Monad/TaggedException/Unsafe.hs
+++ core/src/Control/Monad/TaggedException/Unsafe.hs
@@ -45,6 +45,9 @@ module Control.Monad.TaggedException.Unsafe
 
     -- * Specific unsafe lift operations
     , liftThrow
+    , liftBindLike
+    , liftFlipBindLike
+    , liftKleisliLike
     , liftMask
     )
     where
@@ -100,3 +103,47 @@ liftThrow
     -> e
     -> Throws e (t m) a
 liftThrow = (insideT lift .)
+{-# INLINEABLE liftThrow #-}
+
+-- | Lift operations with type similar to monadic bind. In example:
+--
+-- * @('>>=') :: 'Monad' m => m a -> (a -> m b) -> m b@
+--
+-- * @Prelude.catch :: IO a -> (IOError -> IO a) -> IO a@.
+--
+-- * @Control.Exception.catch :: Exception e => IO a -> (e -> IO a) -> IO a@
+--
+-- Since @1.2.0.0@.
+liftBindLike
+    :: (m a -> (b -> m c) -> m d)
+    -> Throws e m a
+    -> (b -> Throws e m c)
+    -> Throws e m d
+liftBindLike f x g = throwsOne $ f (hideOne x) (hideOne . g)
+{-# INLINE liftBindLike #-}
+
+-- | Lift operations with type similar to flipped monadic bind. In example:
+--
+-- * @('=<<') :: 'Monad' m => (a -> m b) -> m a -> m b@
+--
+-- * @Control.Exception.handle :: Exception e => (e -> IO a) -> IO a -> IO a@
+--
+-- Since @1.2.0.0@.
+liftFlipBindLike
+    :: ((a -> m b) -> m c -> m d)
+    -> (a -> Throws e m b) -> Throws e m c -> Throws e m d
+liftBindLike f g x = throwsOne $ f (hideOne . g) (hideOne x)
+{-# INLINE liftFlipBindLike #-}
+
+-- | Lift klieisli composition like operations. In example:
+--
+-- * @(Control.Monad.>=>) :: 'Monad' m => (a -> m b) -> (b -> m c) -> a -> m c@
+--
+-- * @(Control.Monad.<=<) :: 'Monad' m => (b -> m c) -> (a -> m b) -> a -> m c@
+--
+-- Since @1.2.0.0@.
+liftKleisliLike
+    :: ((a -> m a') -> (b -> m b') -> c -> m c')
+    -> (a -> Throws e m a') -> (b -> Throws e m b') -> c -> Throws e m c'
+liftKleisliLike f g h = throwsOne . f (hideOne . g) (hideOne . h)
+{-# INLINE liftKleisliLike #-}
