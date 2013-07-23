881d9f9c39f7862635e418ad770745bb09a6a4fb
Peter Trško <peter.trsko@gmail.com>
2013-07-23 21:13:49 +0200

    [transformer] UserException: Updated documentation

--- transformer/src/Control/Monad/TaggedException/UserException.hs
+++ transformer/src/Control/Monad/TaggedException/UserException.hs
@@ -10,12 +10,16 @@
 -- Portability:  non-portable (CPP, DeriveDataTypeable)
 --
 -- 'UserException' has instances for 'Exception', and 'HiddenException'
--- classes.   Useful for implementation of @'Monad'('fail')@, and is
--- done so in "Control.Monad.ExceptionT".
+-- classes.  In example it's useful for implementation of @'Monad'('fail')@ and
+-- is done so in "Control.Monad.ExceptionT".
 module Control.Monad.TaggedException.UserException
     (
     -- * UserException
       UserException(..)
+
+    -- ** Examples
+    -- $examples
+
     , emptyUserException
     , mapUserException
     , mapUserException2
@@ -44,36 +48,7 @@ import Control.Monad.Trans.Error (Error(noMsg, strMsg))
 import Control.Monad.TaggedException.Hidden (HiddenException)
 
 
--- | This exception type can be used for implementation of @'Monad'('fail')@.
---
--- Notes on instances:
---
--- * 'Default'('def') is defined as @'def' = 'emptyUserException'@.
---
--- * Show instance returns text "User exception" followed by ": " and message
---   if not empty.
---
--- * 'IsString'('fromString') is defined as @'fromString' = 'UserException'@.
---
--- * 'Monoid'('mempty') is defined as @'mempty' = 'emptyUserException'@ and
---   'Monoid'('mappend') as @'mappend' = 'mapUserException2' ('++')@.
---
--- * If compiled with
---   /semigroups/ <http://hackage.haskell.org/package/semigroups> then
---   'Semigroup'(('<>')) is defined the same way as 'Monoid'('mappend').
---
--- Examples:
---
--- > > show emptyUserException
--- > "User exception"
--- > > show (UserException "foo")
--- > "User exception: foo"
--- > > show (fromString "foo" :: UserException)
--- > "User exception: foo"
--- > > show $ emptyUserException `mappend` UserException "foo"
--- > "User exception: foo"
--- > > show $ UserException "foo" `mappend` UserException "bar"
--- > "User exception: foobar"
+-- | Very simple exception type that only holds message.
 newtype UserException = UserException String
     deriving (Data, Eq, Ord, Typeable)
 
@@ -97,11 +72,14 @@ mapUserException2
 mapUserException2 f (UserException s1) (UserException s2)
     = UserException $ s1 `f` s2
 
+-- | Show instance returns text \"User exception\" followed by \": \" and
+-- message if not empty.
 instance Show UserException where
     showsPrec _ e = (showString "User exception" .) $ case e of
         UserException ""  -> id
         UserException msg -> showString ": " . showString msg
 
+-- | @'def' = 'emptyUserException'@
 instance Default UserException where
     def = emptyUserException
 
@@ -113,14 +91,19 @@ instance Exception UserException
 
 instance HiddenException UserException
 
+-- | @'fromString' = 'UserException'@
 instance IsString UserException where
     fromString = UserException
 
+-- | @'mempty' = 'emptyUserException'@
+--
+-- @'mappend' = 'mapUserException2' ('++')@
 instance Monoid UserException where
     mempty = emptyUserException
     mappend = mapUserException2 (++)
 
 #ifdef VERSION_semigroups
+-- | @('<>')  = 'mapUserException2' ('++') = 'mappend'@
 instance Semigroup UserException where
     (<>) = mapUserException2 (++)
 #if MIN_VERSION_semigroups(0,5,0)
@@ -137,3 +120,16 @@ instance Semigroup UserException where
     -- MIN_VERSION_semigroups(0,8,0) elif MIN_VERSION_semigroups(0,7,0)
 #endif
     -- VERSION_semigroups
+
+-- $examples
+--
+-- > > show emptyUserException
+-- > "User exception"
+-- > > show (UserException "foo")
+-- > "User exception: foo"
+-- > > show (fromString "foo" :: UserException)
+-- > "User exception: foo"
+-- > > show $ emptyUserException `mappend` UserException "foo"
+-- > "User exception: foo"
+-- > > show $ UserException "foo" `mappend` UserException "bar"
+-- > "User exception: foobar"
